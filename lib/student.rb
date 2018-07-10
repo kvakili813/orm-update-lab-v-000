8717572125e0cf8aef91bfd82f00fed91540f604
Kayvan Vakili <kvakili813@gmail.com>
2018-07-10 21:05:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  ttr_accessor :name, :grade
+  attr_accessor :name, :grade
   attr_reader :id
 
   def initialize(name, grade, id=nil)
